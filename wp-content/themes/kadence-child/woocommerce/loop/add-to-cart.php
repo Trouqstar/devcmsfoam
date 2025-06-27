59cc9ac13748aa8ff63003b3f21364d4f17470be
Trouqstar <damian.kawalec31@gmail.com>
2025-06-27 11:06:21 -0400

    Variable Product Support

--- wp-content/themes/kadence-child/woocommerce/loop/add-to-cart.php
+++ wp-content/themes/kadence-child/woocommerce/loop/add-to-cart.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Loop Add to Cart
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/add-to-cart.php.
+ *
+ * @see     https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 9.2.0
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -20,31 +30,60 @@ $aria_describedby = isset( $args['aria-describedby_text'] )
 	? sprintf( 'aria-describedby="woocommerce_loop_add_to_cart_link_describedby_%s"', esc_attr( $product->get_id() ) )
 	: '';
 
-// Build classes for add to cart button
+// Build button class for simple products
 $button_class = isset( $args['class'] ) ? $args['class'] : 'button add_to_cart_button';
 if ( $in_cart ) {
-	$button_class .= ' added'; // <-- Add class if in cart
+	$button_class .= ' added';
 }
 ?>
 
 <div class="action-button-wrap">
-	<?php
-	// Output Add to Cart button
-	echo apply_filters(
-		'woocommerce_loop_add_to_cart_link',
-		sprintf(
-			'<a href="%s" %s data-quantity="%s" class="%s" %s>%s</a>',
-			esc_url( $product->add_to_cart_url() ),
-			$aria_describedby,
-			esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-			esc_attr( $button_class ),
-			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-			'<span class="cart-icon material-symbols-outlined">shopping_bag</span>'
-		),
-		$product,
-		$args
-	);
-	?>
+	<?php if ( $product->is_type( 'variable' ) ) : ?>
+		<!-- VARIABLE PRODUCT: Show attribute selection form -->
+		<form class="variations_form cart" method="post" enctype='multipart/form-data'
+			  action="<?php echo esc_url( $product->get_permalink() ); ?>"
+			  data-product_id="<?php echo absint( $product->get_id() ); ?>"
+			  data-product_variations="<?php echo esc_attr( wp_json_encode( $product->get_available_variations() ) ); ?>">
+
+			<?php
+			$attributes = $product->get_variation_attributes();
+			foreach ( $attributes as $attribute_name => $options ) :
+			?>
+				<div class="variation-select">
+					<select name="attribute_<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>">
+						<option value=""><?php echo wc_attribute_label( $attribute_name ); ?></option>
+						<?php foreach ( $options as $option ) : ?>
+							<option value="<?php echo esc_attr( $option ); ?>"><?php echo esc_html( $option ); ?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+			<?php endforeach; ?>
+
+			<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" />
+			<button type="submit" class="single_add_to_cart_button button alt">
+				<span class="cart-icon material-symbols-outlined">shopping_bag</span>
+			</button>
+		</form>
+
+	<?php else : ?>
+		<!-- SIMPLE PRODUCT: Direct Add to Cart -->
+		<?php
+		echo apply_filters(
+			'woocommerce_loop_add_to_cart_link',
+			sprintf(
+				'<a href="%s" %s data-quantity="%s" class="%s" %s>%s</a>',
+				esc_url( $product->add_to_cart_url() ),
+				$aria_describedby,
+				esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+				esc_attr( $button_class ),
+				isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+				'<span class="cart-icon material-symbols-outlined">shopping_bag</span>'
+			),
+			$product,
+			$args
+		);
+		?>
+	<?php endif; ?>
 
 	<!-- View Cart Button -->
 	<a href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="added_to_cart wc-forward">View cart</a>

fd22ce157ee8b48b79a34ec44f97c712f2808155
Trouqstar <damian.kawalec31@gmail.com>
2025-06-27 10:25:46 -0400

    Product Card CSS Finished

--- wp-content/themes/kadence-child/woocommerce/loop/add-to-cart.php
+++ wp-content/themes/kadence-child/woocommerce/loop/add-to-cart.php
@@ -1,28 +1,35 @@
 <?php
-/**
- * Loop Add to Cart
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/loop/add-to-cart.php.
- *
- * @see     https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 9.2.0
- */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 global $product;
 
+// Check if product is in cart
+$in_cart = false;
+if ( WC()->cart && ! WC()->cart->is_empty() ) {
+	foreach ( WC()->cart->get_cart() as $cart_item ) {
+		if ( $cart_item['product_id'] == $product->get_id() ) {
+			$in_cart = true;
+			break;
+		}
+	}
+}
+
 $aria_describedby = isset( $args['aria-describedby_text'] )
 	? sprintf( 'aria-describedby="woocommerce_loop_add_to_cart_link_describedby_%s"', esc_attr( $product->get_id() ) )
 	: '';
+
+// Build classes for add to cart button
+$button_class = isset( $args['class'] ) ? $args['class'] : 'button add_to_cart_button';
+if ( $in_cart ) {
+	$button_class .= ' added'; // <-- Add class if in cart
+}
 ?>
 
 <div class="action-button-wrap">
 	<?php
-	// Add to Cart button
+	// Output Add to Cart button
 	echo apply_filters(
 		'woocommerce_loop_add_to_cart_link',
 		sprintf(
@@ -30,7 +37,7 @@ $aria_describedby = isset( $args['aria-describedby_text'] )
 			esc_url( $product->add_to_cart_url() ),
 			$aria_describedby,
 			esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-			esc_attr( isset( $args['class'] ) ? $args['class'] : 'button add_to_cart_button' ),
+			esc_attr( $button_class ),
 			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
 			'<span class="cart-icon material-symbols-outlined">shopping_bag</span>'
 		),

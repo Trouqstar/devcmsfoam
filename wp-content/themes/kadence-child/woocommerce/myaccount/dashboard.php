28e038b2541a08025197bc67ffb6d39b85055b29
Trouqstar <damian.kawalec31@gmail.com>
2025-07-04 09:42:38 -0400

    Dashboard Main

--- wp-content/themes/kadence-child/woocommerce/myaccount/dashboard.php
+++ wp-content/themes/kadence-child/woocommerce/myaccount/dashboard.php
@@ -1,81 +1,47 @@
 <?php
-/**
- * My Account Dashboard
- *
- * Shows the first intro screen on the account dashboard.
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/dashboard.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 4.4.0
- */
+defined( 'ABSPATH' ) || exit;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-
-$allowed_html = array(
-	'a' => array(
-		'href' => array(),
-	),
-);
+$current_user = wp_get_current_user();
 ?>
 
-<p>
-	<?php
-	printf(
-		/* translators: 1: user display name 2: logout url */
-		wp_kses( __( 'Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ), $allowed_html ),
-		'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
-		esc_url( wc_logout_url() )
-	);
-	?>
-</p>
+<div class="custom-dashboard">
+    <h2 class="dashboard-heading">My Account</h2>
+    <p class="dashboard-welcome">Hello <?php echo esc_html( $current_user->display_name ); ?></p>
 
-<p>
-	<?php
-	/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
-	$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
-	if ( wc_shipping_enabled() ) {
-		/* translators: 1: Orders URL 2: Addresses URL 3: Account URL. */
-		$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">shipping and billing addresses</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
-	}
-	printf(
-		wp_kses( $dashboard_desc, $allowed_html ),
-		esc_url( wc_get_endpoint_url( 'orders' ) ),
-		esc_url( wc_get_endpoint_url( 'edit-address' ) ),
-		esc_url( wc_get_endpoint_url( 'edit-account' ) )
-	);
-	?>
-</p>
+    <div class="dashboard-grid">
+        <a href="<?php echo esc_url( wc_get_endpoint_url( 'orders' ) ); ?>" class="dashboard-tile">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/icon-orders.svg" alt="">
+            <strong>My orders</strong>
+            <span>View current and past orders</span>
+        </a>
+        <a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-account' ) ); ?>" class="dashboard-tile">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/icon-user.svg" alt="">
+            <strong>Personal details</strong>
+            <span>Manage your name, contact address and phone number</span>
+        </a>
+        <a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address' ) ); ?>" class="dashboard-tile">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/icon-address.svg" alt="">
+            <strong>Address book</strong>
+            <span>Manage your delivery addresses</span>
+        </a>
+        <a href="<?php echo esc_url( wc_get_endpoint_url( 'payment-methods' ) ); ?>" class="dashboard-tile">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/icon-card.svg" alt="">
+            <strong>Payment details</strong>
+            <span>Manage your payment methods</span>
+        </a>
+        <a href="#" class="dashboard-tile">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/icon-preferences.svg" alt="">
+            <strong>My preferences</strong>
+            <span>Manage how we contact you</span>
+        </a>
+        <a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-account' ) ); ?>" class="dashboard-tile">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/icon-password.svg" alt="">
+            <strong>Manage password</strong>
+            <span>Update your password</span>
+        </a>
+    </div>
 
-<?php
-	/**
-	 * My Account dashboard.
-	 *
-	 * @since 2.6.0
-	 */
-	do_action( 'woocommerce_account_dashboard' );
-
-	/**
-	 * Deprecated woocommerce_before_my_account action.
-	 *
-	 * @deprecated 2.6.0
-	 */
-	do_action( 'woocommerce_before_my_account' );
-
-	/**
-	 * Deprecated woocommerce_after_my_account action.
-	 *
-	 * @deprecated 2.6.0
-	 */
-	do_action( 'woocommerce_after_my_account' );
-
-/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
+    <p class="dashboard-logout">
+        <a href="<?php echo esc_url( wc_logout_url() ); ?>">Sign out &gt;</a>
+    </p>
+</div>
